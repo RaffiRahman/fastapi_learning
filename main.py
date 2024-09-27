88936aad47e1f526fa3c36f8ce616465b43bd32a
RaffiRahman <rafirahmanit21@gmail.com>
2024-09-27 13:32:16 +0600

    fifth commit

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
+from os import name
 from fastapi import FastAPI, Request 
 from exceptions import StoryException
-from router import blog_get, blog_post, user, article, product, file
+from router import blog_get, blog_post, user, article, product, file, dependencies
 from auth import authentication
 from templates import templates
 from db import models
 from db.database import engine
-from fastapi.responses import JSONResponse
+from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
+import time
+from client import html
+from fastapi.websockets import WebSocket
 
 app = FastAPI()
+app.include_router(dependencies.router)
 app.include_router(templates.router)
 app.include_router(authentication.router)
 app.include_router(file.router)
@@ -29,7 +34,30 @@ def index():
 #         content= {'detail': exc.name}
 #     )
 
+@app.get("/")
+async def get():
+    return HTMLResponse(html)
+
+clients = []
+
+@app.websocket("/chat")
+async def websockets_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    clients.append(websocket)
+    while True:
+        data = await websocket.receive_text()
+        for client in clients:
+            await client.send_text(data)
+
 models.Base.metadata.create_all(engine)
+
+@app.middleware("http")
+async def add_middleware(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    duration = time.time() - start_time
+    response.headers['duration'] = str(duration)
+    return response
 
 app.mount('/files', StaticFiles(directory= "files"), name='files')
 app.mount('/static', 
--- templates/templates.py
+++ templates/templates.py
@@ -1,10 +1,10 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, BackgroundTasks
 from fastapi import responses
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from fastapi.requests import Request
-
 from schemas import ProductBase
+from custom_log import log
 
 router = APIRouter(
     prefix='/templates',
@@ -14,7 +14,11 @@ router = APIRouter(
 templates = Jinja2Templates(directory= "templates")
 
 @router.post("/products/{id}", response_class= HTMLResponse)
-def get_product(id: str, product: ProductBase, request: Request):
+def get_product(id: str, 
+                product: ProductBase, 
+                request: Request, 
+                bt: BackgroundTasks):
+    bt.add_task(log_template_call, f"Template read for product with id {id}")
     return templates.TemplateResponse(
         "product.html",
         {
@@ -25,3 +29,6 @@ def get_product(id: str, product: ProductBase, request: Request):
             "price": product.price
         }
     )
+
+def log_template_call(message: str):
+    log("MyAPI", message)
