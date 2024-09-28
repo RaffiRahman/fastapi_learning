40f39711297e7b1bd74422acb3ab2dcfa8baaa74
RaffiRahman <rafirahmanit21@gmail.com>
2024-09-28 12:41:55 +0600

    sixth and final commit

--- router/dependencies.py
+++ router/dependencies.py
@@ -1,20 +1,30 @@
 from fastapi import APIRouter
 from fastapi.param_functions import Depends
 from fastapi.requests import Request
+from custom_log import log
 
 router = APIRouter(
     prefix='/dependencies',
-    tags=['dependencies']
+    tags=['dependencies'],
+    dependencies= [Depends(log)]
 )
+def convert_params(request: Request, separator: str):
+    query = []
+    for key, value in request.query_params.items():
+        query.append(f"{key} {separator} {value}")
+    return query
 
-def convert_headers(request: Request, separator: str = '--'):
+def convert_headers(request: Request, separator: str = '--', query = Depends(convert_params)):
     out_headers = []
     for key, value in request.headers.items():
         out_headers.append(f" {key} {separator} {value}")
-    return out_headers
+    return {
+        'headers': out_headers,
+        'query': query
+    }
 
 @router.get('')
-def get_items(separator: str = '--',  headers = Depends(convert_headers)):
+def get_items(test: str, separator: str = '--',  headers = Depends(convert_headers)):
     return {
         'items': ['a', 'b', 'c'],
         'headers': headers
@@ -25,4 +35,17 @@ def create_item(headers = Depends(convert_headers)):
     return {
         'result':'new item created',
         'headers': headers
+    }
+
+class Account:
+    def __init__(self, name: str, email: str):
+        self.name = name
+        self.email = email
+
+@router.post('/user')
+def create_user(account: Account = Depends()):
+    # account perform whatever operations
+    return{
+        'name': account.name,
+        'email': account.email
     }
